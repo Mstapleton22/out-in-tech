acc5713d55bbfab01b54953886e6e0e49bdbb849
Mstapleton22 <mike.stapleton2@gmail.com>
2020-02-04 23:19:31 -0700

    events page

--- out-in-tech-website/src/components/Events.js
+++ out-in-tech-website/src/components/Events.js
@@ -5,32 +5,15 @@ function Events() {
   return (
     <div>
       <section className="mission">
-
         <div className="row ">
-          <div className="col-lg-2"></div>
-
-          {/* <div className="col-lg-4 mission-word">
-            MISSION
-          </div> */}
-          <div className="mission-info col-lg-8 justify-content-center">
-            <div className="mission">
-              <h2>Our mission is to create space for Queer individuals who identify as Techies through networking, panels, and other social events.
-            </h2>
-            </div>
-          </div>
-          <div className="col-lg-2"></div>
-        </div>
-        {/* </section>
-      <section className="events-section"> */}
-        {/* <div className="col-lg-2"></div> */}
-
-        <div className="row event-row ">
-          <a className="event-link" href="" target="_blank">
-            <div className="events">
-              Upcoming Event
-          </div >
-          </a>
           {/* <div className="col-lg-2"></div> */}
+          <div className="events-header">Events</div>
+        </div>
+        {/* <div className="col-lg-2"></div> */}
+        <div className="row event-row ">
+          <a className="event-link" href="https://www.eventbrite.com/e/out-in-tech-denver-diversity-and-inclusion-in-tech-tickets-93019404543" target="_blank">
+            <img className="event-image" src="februaryevent.png" />
+          </a>
         </div>
       </section>
     </div>
@@ -38,3 +21,11 @@ function Events() {
 }
 
 export default Events;
+
+{/* <div className="mission-info col-lg-8 justify-content-center">
+            <div className="mission">
+              <h2>Our mission is to create space for Queer individuals who identify as Techies through networking, panels, and other social events.
+            </h2>
+            </div>
+          </div>
+          <div className="col-lg-2"></div>*/}
